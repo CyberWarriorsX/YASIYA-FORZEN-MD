d2cbdaa45c4c8e4fb92ceddb8a827aef2d31d49e
Cyber Srimal <145316619+CyberWarriorsX@users.noreply.github.com>
2023-10-30 10:37:12 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,47 @@
+
+S<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>PRABATH-MD</title>
+
+  </style>
+  <link rel="icon" type="image/x-icon" href="https://i.ibb.co/7YGn5qS/20230305-135924.jpg">
+  <link rel="stylesheet" href="style.css">
+  <link rel="shortcut icon" href="https://i.ibb.co/7YGn5qS/20230305-135924.jpg" sizes="192x192" type="image/png">
+</head>
+  <body><div>
+  <body><div>
+  <a href="https://replit.com/@yasithasamujit2/FORZEN-MD?v=1/" class="btn">SCAN-QR<span><ion-icon name="qr-code-outline"></ion-icon></span></a>
+  <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+  <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+  <br>
+  <br>
+  <a href="https://railway.app/new" class="btn">MONGODB_URL<span><ion-icon name="open-outline"></ion-icon></span></a>
+  <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+  <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+  <br>
+  <br>
+  <a href="https://app.koyeb.com/apps/deploy?type=git&repository=github.com/yasiyaofc/FORZEN-MD&branch=main&env[SESSION_ID]&env[OWNER_NUMBER]=94760018802&env[MONGODB_URI]&&env[OWNER_NAME]=prabath&env[KOYEB_API]&env[AUTO_READ_STATUS]=false&env[ANTI_BAD_WORD]=fuck,huththa,pakaya&env[ALWAYS_ONLINE]=true&env[PREFIX]=.&env[ALIVE_IMG]=https://telegra.ph/file/0ff686352c51b20af8231.jpg&env[ALIVE_MSJ]=IAmOnline&env[AUTO_RESTART]=true&env[SUDO]=94762280384&env[READ_MESSAGE]=false&env[DISABLE_PM]=false&env[WORK_TYPE]=public&env[THEME]=PRABATH-MD&env[PACK_INFO]=prabath;madebyprabathmd&name=prabath-md&env[KOYEB_NAME]=prabath-md&env[ANTILINK_VALUES]=chat.whatsapp.com&env[DL_SIZE]=70&env[PORT]=8000" class="btn">KOYEB<span><ion-icon name="open-outline"></ion-icon></span></a>
+  <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+  <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+  <br>
+  <br>
+  <a href="https://dashboard.heroku.com/new?button-url=https://github.com/yasiyaofc/FORZEN-MD&template=https://github.com/yasiyaofc/FORZEN-MD.git" class="btn">HEROKU<span><ion-icon name="open-outline"></ion-icon></span></a>
+  <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+  <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+  <br>
+  <br>
+  <a href="https://railway.app/template/NO4jvb?referralCode=iM43z3" class="btn">RAILWAY<span><ion-icon name="open-outline"></ion-icon></span></a>
+  <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+  <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+<footer class="footer">
+  <p>FORZEN-MD<br>
+  <a href="https://chat.whatsapp.com/L2i6oDCjljt4mtBTUvTh7t">SUPPORT GROUP</a></p>
+</footer>
+</body>
+</html>
